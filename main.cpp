6b6a0045ed9f8a2bcdbb94212dbb8f9859eb5ac2
Santiago Ortiz <s.ortiz2@udea.edu.co>
2025-04-16 16:06:34 -0500

    Solucion entradas y formato de las rutas

--- main.cpp
+++ main.cpp
@@ -11,16 +11,16 @@ int main(){
     string rutaInputI_D,rutaInputM,rutaInputI_M,rutaInputI_O; // rutas de las imagenes
     int weightI_D,heightI_D,weightM,heightM,weightI_M,heightI_M; // anchos y largos de las imagenes
     unsigned int nTxt;// numero de txts
-    cout << "Ingrese la ruta de la imagen P: "; // ENTRADAS RUTAS
-    cin >> rutaInputI_D;
+    cout << "Ingrese la ruta de la imagen I_D: "; // ENTRADAS RUTAS
+    getline(cin, rutaInputI_D);
     QString rutaI_D = QString::fromStdString(rutaInputI_D);
     unsigned char* ptrI_D = loadPixels(rutaI_D, weightI_D, heightI_D); // apuntador hacia datos imagen
     cout << "Ingrese la ruta de la imagen M: ";
-    cin >> rutaInputM;
+    getline(cin, rutaInputM);
     QString rutaM = QString::fromStdString(rutaInputM);
     unsigned char* ptrM = loadPixels(rutaM, weightM, heightM);
     cout << "Ingrese la ruta de la imagen I_M: ";
-    cin >> rutaInputI_M;
+    getline(cin, rutaInputI_M);
     QString rutaI_M = QString::fromStdString(rutaInputI_M);
     unsigned char* ptrI_M = loadPixels(rutaI_M, weightI_M, heightI_M);
     cout << "Ingrese la cantidad de archivos txt: ";
@@ -31,20 +31,20 @@ int main(){
         int nPixeles;
         string rutaInputTxt;
         cout << "Ingrese la ruta del txt correspondiente: "; // ENTRADAS RUTAS
-        cin >> rutaInputTxt;
-        const char* rutaTxt = rutaInputTxt.c_str();
-        unsigned int* ptrTxt = loadSeedMasking(rutaTxt, seed, nPixeles); // apuntador hacia datos txt // (primer parametro verificar)
+        cin.ignore();
+        getline(cin, rutaInputTxt);
+        unsigned int* ptrTxt = loadSeedMasking(rutaInputTxt.c_str(), seed, nPixeles); // apuntador hacia datos txt // (primer parametro verificar)
         restarArreglos(ptrTxt, ptrM, nPixeles);
         transformacion = identificarTransformacion(ptrTxt,ptrI_D,seed);
-        transformacionInversa(ptrI_D,transformacion);
+        transformacionInversa(ptrI_D,transformacion,weightI_D,heightI_D);
         delete ptrTxt;
     }
     cout << "Ingrese la ruta de la foto a guardar: ";
-    cin >> rutaInputI_O;
+    getline(cin, rutaInputI_O);
     QString rutaI_O = QString::fromStdString(rutaInputI_O);
     cout << exportImage(ptrI_D, weightI_D, heightI_D, rutaI_O);
-    delete ptrI_D;
-    delete ptrM;
-    delete ptrI_M;
+    delete [] ptrI_D;
+    delete [] ptrM;
+    delete [] ptrI_M;
     return 0;
 }
